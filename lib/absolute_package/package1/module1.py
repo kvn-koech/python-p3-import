30de4f963cf40a2d651d5ed78656c03911855662
Kevin <kevinkoech007@gmail.com>
2025-09-30 13:35:35 +0300

    Add function2 to module1 and create module2 with func2

--- lib/absolute_package/package1/module1.py
+++ lib/absolute_package/package1/module1.py
@@ -1,2 +1,5 @@
 def function1():
     print('Function 1 in module 1')
+    
+def function2():
+    print('Function 2 in module 1')
---
+++ lib/absolute_package/package1/module2.py
@@ -0,0 +1,5 @@
+from absolute_package.package1.module1 import *
+
+def func2():
+    return "Function 2 from module2"
+
